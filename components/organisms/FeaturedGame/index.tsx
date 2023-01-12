1440a2528e1ec4fdac8dba00eb0d080022e457a2
Asya'ri ‎ <asyari818@gmail.com>
2023-01-12 14:32:28 +0700

    Create API Data Types

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 import GameItem from "../../molecules/GameItems";
 import axios from "axios";
 import { getFeaturedGame } from "../../../services/player";
+import { GameItemTypes } from "../../../services/data-types";
 
 export default function FeaturedGame() {
   const [gameList, setGameList] = useState([]);
@@ -14,7 +15,9 @@ export default function FeaturedGame() {
   useEffect(() => {
     getFeatureGameList();
   }, []);
-  
+
+  const API_IMG = process.env.NEXT_PUBLIC_API_IMG;
+
   return (
     <section className="featured-game pt-50 pb-50">
       <div className="container-fluid">
@@ -26,11 +29,14 @@ export default function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          {gameList.map(item => {
-            return (
-            <GameItem key={item._id} title={item.name} category={item.category.name} thumbnail={`http://localhost:4000/uploads/${item.thumbnail}`}/>
-            )
-          })}
+          {gameList.map((item: GameItemTypes) => (
+            <GameItem
+              key={item._id}
+              title={item.name}
+              category={item.category.name}
+              thumbnail={`${API_IMG}/uploads/${item.thumbnail}`}
+            />
+          ))}
         </div>
       </div>
     </section>
