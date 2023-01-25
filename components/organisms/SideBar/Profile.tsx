7a3bbaa8ce8eb1c0949d2fbc9a329ddd7fa00571
Asya'ri ‎ <asyari818@gmail.com>
2023-01-25 14:08:48 +0700

    Add border radius in image

--- components/organisms/SideBar/Profile.tsx
+++ components/organisms/SideBar/Profile.tsx
@@ -26,6 +26,7 @@ export default function Profile() {
         height={90}
         className="img-fluid mb-20"
         alt="profile"
+        style={{ borderRadius: "100%"}}
       />
       <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
       <p className="color-palette-2 m-0">{user.email}</p>
